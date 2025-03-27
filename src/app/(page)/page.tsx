8bc580cd123b2c6b930d118fa311a5adc0788550
HOCKIIE <suphawat@ignite-idea.com>
2025-03-27 22:14:58 +0700

    upgrade next version % home page

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -4,7 +4,7 @@ import AboutMeSection from "@/components/main/section/AboutMe";
 
 export default function Home() {
     return (
-        <div className="container mx-auto px-2 xl:px-0">
+        <div className="container px-2 xl:px-4">
             <div className="pt-20 pb-10">
                 <AboutMeSection />
                 <ContactSection />
