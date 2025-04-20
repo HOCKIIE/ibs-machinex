5b1a0a8b43d8f8954e94de7bf21becb2ecdfdf45
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2025-04-20 12:53:46 +0700

    feat: Implement Brand and About stores with CRUD operations and API integration

--- src/store/useAboutStore.tsx
+++ src/store/useAboutStore.tsx
@@ -1,14 +1,13 @@
-"use client";
 import Api from "@/services/Api";
 import { create } from "zustand";
 import toast from "react-hot-toast";
-import { BrandState, ApiResponse } from "@/types/BrandType";
+import { AboutState, ApiResponse } from "@/types/AboutType";
 
-const apiPrefix = '/contact';
-const prefix = '/admin/contact';
+const apiPrefix = '/about';
+const prefix = '/admin/about';
 
-export const useBrandStore = create<BrandState>((set) => ({
-    brands: null,
+export const useAboutStore = create<AboutState>((set) => ({
+    about: null,
     isLoading: false,
     response:{ status:null,message:null},
     error: null,
@@ -22,7 +21,7 @@ export const useBrandStore = create<BrandState>((set) => ({
         try {
             const response = await Api.get<ApiResponse>(apiPrefix);
             set({
-                brands: { 
+                about: { 
                     ...response.data, 
                     created_at: (response.data as { created_at?: string }).created_at || "", 
                     updated_at: (response.data as { updated_at?: string }).updated_at || "" 
@@ -35,32 +34,12 @@ export const useBrandStore = create<BrandState>((set) => ({
             toast.error(errorMessage);
         }
     },
-
-    createData: async (data, router) => {
-        set({ isLoading: true, error: null });
-        try {
-            const response = await Api.post<ApiResponse>(`${apiPrefix}/create`, data);
-            set({
-                brands: response.data,
-                isLoading: false,
-            });
-            toast.success("The User was created successfully!");
-            setTimeout(() => {
-                router.push(prefix);
-            }, 1000);
-        } catch (error: unknown) {
-            const response = (error as { response?: { data?: { errors?: Record<string, string[]>; message?: string } } })?.response;
-            const errorMessage = response?.data?.message || "An unknown error occurred";
-            toast.error(errorMessage);
-        }
-    },
-
     updateData: async (id, data, router) => {
         set({ isLoading: true, error: null });
         try {
             const response = await Api.put<ApiResponse>(`${apiPrefix}/update`, data);
             set({
-                brands: response.data,
+                about: response.data,
                 isLoading: false,
             });
             toast.success("The User was updated successfully!");
@@ -72,6 +51,5 @@ export const useBrandStore = create<BrandState>((set) => ({
             const errorMessage = response?.data?.message || "An unknown error occurred";
             toast.error(errorMessage);
         }
-    },
-
-}));
+    }
+});
