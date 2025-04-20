9bdb3af937f5d3c05012c271ebdf325c39f7aae4
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2025-04-20 12:53:46 +0700

    feat: Implement Brand and About stores with CRUD operations and API integration

--- src/types/AboutType.tsx
+++ src/types/AboutType.tsx
@@ -3,4 +3,28 @@ export interface AboutType {
     detail_th:string;
     detail_en:string;
     detail_jp:string;
+
+    created_at:string;
+    updated_at:string;
+}
+
+export interface ApiResponse {
+    id:string;
+    detail_th:string;
+    detail_en:string;
+    detail_jp:string;
+}
+
+export interface AboutState {
+
+    about: AboutType[];
+    isLoading: boolean;
+    error: string | null;
+    token: string | null;
+    id: string;
+    role: string;
+
+    fetchData: () => Promise<void>;
+    updateData : (id: string, data: AboutType, router: any) => Promise<void>;
+
 }
