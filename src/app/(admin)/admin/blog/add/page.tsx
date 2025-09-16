9c759991bac1079b92fb31e9bec743fca1698ce4
HOCKIIIE <suphawat@ignite-idea.com>
2025-09-16 16:51:25 +0700

    feat: enhance blog page components; improve data fetching logic and state management in BlogForm

--- src/app/(admin)/admin/blog/add/page.tsx
+++ src/app/(admin)/admin/blog/add/page.tsx
@@ -24,7 +24,7 @@ export default function Page(){
         detail_en: "",
         detail_ja: "",
         status: false,
-        pathName:"",
+        pathName: "",
         category: [],
         categories: [],
         published_at:"",
