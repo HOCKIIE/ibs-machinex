adb9d86934a7667c4f3f4d8fa844e9ce467c216a
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2025-10-09 12:50:08 +0700

    feat: adjust API_URL variable in Api.tsx

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -17,7 +17,12 @@ export const setAccessToken = (token: string | null) => {
     accessToken = token;
 };
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ??
+    (
+        process.env.NODE_ENV === "development"
+        ? "http://localhost:8000/"
+        : ""
+    );
 const Api = axios.create({baseURL:`${API_URL}/api`});
 
 const publicRoutes = [
