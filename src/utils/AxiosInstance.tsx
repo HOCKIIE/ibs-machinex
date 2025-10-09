a24831a20c86af67245608639ac199d34fe36678
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2025-10-09 15:54:37 +0700

    feat: changed  API_URL variable in AxiosInstance.tsx

--- src/utils/AxiosInstance.tsx
+++ src/utils/AxiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_URL = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_API_URL_DEV : process.env.NEXT_PUBLIC_API_URL_PROD;
 
 const AxiosInstance = axios.create({
     baseURL: `${API_URL}/api`,
