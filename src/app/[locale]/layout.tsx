87152a5a3186b7a62c17fadd6a8b4d1b6be3a168
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2026-01-07 17:15:46 +0700

    feat: add Google site verification meta tag to layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -54,6 +54,10 @@ export default async function RootLayout({children}:{children: React.ReactNode})
         <html lang="en">
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="google-site-verification"
+                    content="googleff2bcb4da7296a98.html"
+                />
             </Head>
             <PageSettingsContext>
                 <NextIntlClientProvider messages={messages} >
