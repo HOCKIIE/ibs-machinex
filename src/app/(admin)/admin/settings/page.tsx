065f30b48fbc69570a1dd62e49ff38988681f351
hockiie <68018628+HOCKIIE@users.noreply.github.com>
2026-01-07 11:32:00 +0700

    refactor: simplify video URL handling in useIntroVideoStore and remove unused environment variables

--- src/app/(admin)/admin/settings/page.tsx
+++ src/app/(admin)/admin/settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React,{ useState, useEffect, useRef } from 'react';
+import React,{ useEffect, useRef } from 'react';
 import Api from '@/services/Api';
 import DefaultLayout from '@/components/admin/layout/DefaultLayout';
 import Breadcrumb from '@/components/admin/Breadcrumb/Breadcrumb';
--- src/store/useIntroVideoStore.tsx
+++ src/store/useIntroVideoStore.tsx
@@ -5,10 +5,6 @@ import { create } from "zustand";
 import { ProcessToast } from "@/utils/ProcessToast";
 
 const apiPrefix = '/admin/settings';
-const env = process.env.NEXT_PUBLIC_ENV;
-const prefix = env === "development" 
-    ? process.env.NEXT_PUBLIC_PREFIX_DEV 
-    : process.env.NEXT_PUBLIC_PREFIX_PROD;
 
 type IntroVideoState = {
     response: { status: null, statusCode: number|null, message: null, path: string|null };
@@ -24,12 +20,7 @@ export const useIntroVideoStore = create<IntroVideoState>((set) => ({
     response: { status: null , statusCode: null, message: null, path:null },
     videoUrl: null,
     videoFile: null,
-    setVideoUrl: (url) => {
-        if (env === "development")
-            set({ videoUrl: url })
-        else
-            set({ videoUrl: (prefix ? '/'+prefix:'')+url })
-    },
+    setVideoUrl: (url) => set({ videoUrl: url }),
     setVideoFile: (file) => set({ videoFile: file }),
     resetToOld: () => set({videoFile: null}),
 
